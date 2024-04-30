faf5129b4d91cd4344c49064ba1a3a0770679189
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-04-30 14:29:26 +0500

    Add files via upload

---
+++ 13-Testing_Sets/app.js
@@ -0,0 +1,21 @@
+
+var weight = prompt ("Weight");
+
+if(weight > 300 && time <6){
+    alert("come to our tryout");
+}
+else{
+    alert("come to our cookout");
+}
+
+
+var coursename = prompt ("Enter Your CourseName");
+
+if (coursename === "moblie reparing" || coursename === "graphic" || coursename === "webdevelopment"){
+    console.log("You 're enrolled")
+}
+else {
+    console.log("You 're not enrolled")
+}
+
+
